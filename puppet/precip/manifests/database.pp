b80f9860727a58ad67976ab5abc559c756500f8c
Rob Wilmshurst <rob.wilmshurst@tuidev.com>
2017-01-26 13:05:55 +0000

    Override MySQL config for better perf

--- puppet/precip/manifests/database.pp
+++ puppet/precip/manifests/database.pp
@@ -35,13 +35,32 @@ class precip::database {
   class { 'mysql::server':
     package_name => 'percona-server-server-5.5',
     package_ensure => 'latest',
-    override_options => { 
-      'mysqld' => { 
+    override_options => {
+      'mysqld' => {
         'bind-address' => '0.0.0.0',
-        'log_error' => '/vagrant/log/mysql_error.log'
+        'log_error' => '/vagrant/log/mysql_error.log',
+        'key_buffer_size' => '20M',
+        'max_allowed_packet' => '100M',
+        'query_cache_limit' => '5M',
+        'query_cache_size' => '40M',
+        'thread_cache_size' => '200',
+        'innodb_file_format' => 'Barracuda',
+        'innodb_file_format_max' => 'Barracuda',
+        'innodb_file_per_table' => '1',
+        'group_concat_max_len' => '10240',
       },
       'mysqld_safe' => {
-        'log_error' => '/vagrant/log/mysql_error.log'
+        'bind-address' => '0.0.0.0',
+        'log_error' => '/vagrant/log/mysql_error.log',
+        'key_buffer_size' => '20M',
+        'max_allowed_packet' => '100M',
+        'query_cache_limit' => '5M',
+        'query_cache_size' => '40M',
+        'thread_cache_size' => '200',
+        'innodb_file_format' => 'Barracuda',
+        'innodb_file_format_max' => 'Barracuda',
+        'innodb_file_per_table' => '1',
+        'group_concat_max_len' => '10240',
       }
     },
     require => [
